02356c247edb6c9f7fee42a7cc8858c7bad6fdc0
Siddharta Mohapatra <siddharthmohapatra2004@gmail.com>
2026-05-03 12:41:27 +0530

    darkmode

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,9 +1,25 @@
-function App(){
-    return (
-        <div>
-            
-        </div>
-    )
+//component
+//usestate
+
+import { useState } from "react";
+const App =()=> {
+  
+  const [darkmode, setDarkmode] = useState(false)
+
+  function onToggleTheme() {
+    setDarkmode(!darkmode)
+    console.log(darkmode);
+    
+  }
+
+  return (
+    <div style={{
+        height: "100vh",
+        backgroundColor: darkmode ? "#121212": "#ffff"
+    }}>
+        <button onClick={onToggleTheme}>Toggle</button>
+    </div>
+  )
 }
 
-export default App
+export default App;
