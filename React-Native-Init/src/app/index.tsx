e7a8511f2c79992b06249f6cd7fa4ff04b456b68
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-07-20 20:41:45 +0530

    updated

---
+++ React-Native-Init/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, Image } from "react-native";
+
+export default function Homescreen() {
+  return (
+    <View>
+      {" "}
+      <Text numberOfLines={5}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, aliquid ad. Maxime a blanditiis quas quos id, rem ratione at unde dolor minima quidem enim ipsa, sunt adipisci quisquam impedit vero nostrum dolorum omnis quis voluptatibus quaerat culpa iure? Temporibus repellendus a eaque velit, laudantium voluptate sit perspiciatis dolorells laboriosam.</Text>{" "}
+    </View>
+  );
+}
