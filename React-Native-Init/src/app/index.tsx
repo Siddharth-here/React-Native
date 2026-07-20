76f4f0413e7a007ca07273f6fbbcbc152f8ebca6
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-07-20 21:21:54 +0530

    pressable props

--- React-Native-Init/src/app/index.tsx
+++ React-Native-Init/src/app/index.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import { View, Text, Image,TextInput } from "react-native";
+import { View, Text, Image, TextInput, Pressable } from "react-native";
 
 export default function Homescreen() {
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
   return (
     <View>
       {" "}
       <Text numberOfLines={5}>
-         laudantium voluptate sit perspiciatis
-        dolorells laboriosam.
+        laudantium voluptate sit perspiciatis dolorells laboriosam.
       </Text>{" "}
       {/* remote image*/}
       <Image
@@ -27,20 +26,28 @@ export default function Homescreen() {
         }}
         blurRadius={50}
       />
-
-      <TextInput 
-      placeholder="enter your name" 
-      value={name} 
-      onChangeText={setName}
-      placeholderTextColor={"red"}
-      style={{
-        borderWidth:1,
-        borderColor:"#ddd",
-        marginTop:10,
-        fontSize:24,
-      }}
-      
+      <TextInput
+        placeholder="enter your name"
+        value={name}
+        onChangeText={setName}
+        placeholderTextColor={"red"}
+        style={{
+          borderWidth: 1,
+          borderColor: "#ddd",
+          marginTop: 10,
+          fontSize: 24,
+        }}
       />
+      <Pressable
+        onPress={() => alert("button pressed")}
+        style={({ pressed }) => ({
+          backgroundColor: pressed ? "#0e4e0f" : "#6c63ff",
+        })}
+      >
+        {({ pressed }) =>
+          pressed ? <Text>Pressing...</Text> : <Text>Press me</Text>
+        }
+      </Pressable>
     </View>
   );
 }
